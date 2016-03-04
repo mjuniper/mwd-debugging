d41ecb9bd251088b9851f662fa489be81e64437b
mjuniper <mjuniper@gmail.com>
2016-03-04 15:48:59 -0700

    add readme, tweak js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,8 +3,8 @@ $(() => {
   $.getJSON('https://opendata.arcgis.com/api/v2/datasets')
     .then(resp => {
       const target = $('table tbody');
+      const elAry = [];
       let newEl;
-      let elAry = [];
       for (let d of resp.data) {
         // TODO: also bust it by misaligning the attributes
         newEl = $(`<tr><td>${d.attributes.name}</td><td>${d.attributes.itemType}</td><td>${d.attributes.quality}</td><td>${d.attributes.recordCount}</td></tr>`);
