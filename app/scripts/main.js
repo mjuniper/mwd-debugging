8e109908544bcd29968e8d553e5ee1d9a29521bb
mjuniper <mjuniper@gmail.com>
2016-03-05 22:10:26 -0700

    finished working app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,33 @@
 $(() => {
-  // TODO: bust it by using http
-  $.getJSON('https://opendata.arcgis.com/api/v2/datasets')
-    .then(resp => {
-      const target = $('table tbody');
-      const elAry = [];
-      let newEl;
-      for (let d of resp.data) {
-        // TODO: also bust it by misaligning the attributes
-        newEl = $(`<tr><td>${d.attributes.name}</td><td>${d.attributes.itemType}</td><td>${d.attributes.quality}</td><td>${d.attributes.recordCount}</td></tr>`);
-        // TODO: also bust it by omitting one of these lines
-        elAry.push(newEl);
-        target.append(elAry);
-      }
-    });
+  const API = 'https://opendata.arcgis.com/api/v2';
+  let page = 0;
+  let size = 10;
+
+  nextPage();
+
+  function nextPage() {
+    page += 1;
+    fetch().then(handleResponse);
+  }
+
+  function fetch() {
+    const url = `${API}/datasets?page[number]=${page}&page[size]=${size}`;
+    return $.getJSON(url)
+      .then(resp => {
+        return resp.data;
+      });
+  }
+
+  function handleResponse(data) {
+    const target = $('table tbody').empty();
+    const elAry = [];
+    let newEl;
+    for (let d of data) {
+      newEl = $(`<tr><td>${d.attributes.name}</td><td>${d.attributes.itemType}</td><td>${d.attributes.quality}</td><td>${d.attributes.recordCount}</td></tr>`);
+      elAry.push(newEl);
+      target.append(elAry);
+    }
+  }
+
+  $('#more').on('click', nextPage);
 });
